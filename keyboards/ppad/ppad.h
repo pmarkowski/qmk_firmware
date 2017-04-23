3580e63233d7b152da9e25fa2aab40384d108b5b
Philip <philipmarkowski642@gmail.com>
2017-04-22 20:05:03 -0400

    Add first ppad keymap

---
+++ keyboards/ppad/ppad.h
@@ -0,0 +1,18 @@
+#ifndef PPAD_H
+#define PPAD_H
+
+#define KEYMAP( \
+    k00, k01, k02, k03, \
+    k10, k11, k12, k13, \
+    k20, k21, k22, \
+    k30, k31, k32, k33, \
+    k40,      k42 \
+) { \
+    { k00, k01, k02, k03, }, \
+    { k10, k11, k12, k13, }, \
+    { k20, k21, k22, KC_NO, }, \
+    { k30, k31, k32, k33, }, \
+    { k40, KC_NO, k42, KC_NO } \
+}
+
+#endif
