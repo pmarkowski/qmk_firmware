4d8b34d950b380d86efc58be62641497fcd4ce74
pmarkowski <pmarkowski@users.noreply.github.com>
2017-04-27 07:09:41 -0400

    Update keymap.c

--- keyboards/ppad/keymaps/default/keymap.c
+++ keyboards/ppad/keymaps/default/keymap.c
@@ -1,5 +1,7 @@
 #include "ppad.h"
 
+#define _____ KC_TRANS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = KEYMAP(
         KC_NLCK,    KC_PSLS,    KC_PAST,    KC_PMNS, \
@@ -7,7 +9,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P4,      KC_P5,      KC_P8,               \
         KC_P1,      KC_P2,      KC_P3,      KC_PENT, \
         KC_P0,                  KC_PDOT
-    )
+    ),
+    [1] = KEYMAP(
+        KC_NLCK,    KC_PSLS,    KC_PAST,    KC_PMNS, \
+        _____,      _____,      _____,      KC_PPLS, \
+        _____,      _____,      _____,               \
+        _____,      _____,      _____,      KC_PENT, \
+        _____,                  KC_PDOT
+    ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
