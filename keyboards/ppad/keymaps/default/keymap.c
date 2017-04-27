23d42b79d61ce342206f991260770060353411d6
pmarkowski <pmarkowski@users.noreply.github.com>
2017-04-27 13:07:15 -0400

    Update keymap.c
    Ppad add proper defines, more proper layer, NumLock as momentary toggle

--- keyboards/ppad/keymaps/default/keymap.c
+++ keyboards/ppad/keymaps/default/keymap.c
@@ -1,21 +1,24 @@
 #include "ppad.h"
 
-#define _____ KC_TRANS
+#define _____ KC_TRNS
+
+#define _NUM 0
+#define _MED 1
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = KEYMAP(
-        KC_NLCK,    KC_PSLS,    KC_PAST,    KC_PMNS, \
-        KC_P7,      KC_P8,      KC_P9,      KC_PPLS, \
-        KC_P4,      KC_P5,      KC_P8,               \
-        KC_P1,      KC_P2,      KC_P3,      KC_PENT, \
-        KC_P0,                  KC_PDOT
+    [_NUM] = KEYMAP(
+        LT(_MED, KC_NLCK),    KC_PSLS,    KC_PAST,    KC_PMNS, \
+        KC_P7,                KC_P8,      KC_P9,      KC_PPLS, \
+        KC_P4,                KC_P5,      KC_P8,               \
+        KC_P1,                KC_P2,      KC_P3,      KC_PENT, \
+        KC_P0,                            KC_PDOT
     ),
-    [1] = KEYMAP(
-        KC_NLCK,    KC_PSLS,    KC_PAST,    KC_PMNS, \
-        _____,      _____,      _____,      KC_PPLS, \
-        _____,      _____,      _____,               \
-        _____,      _____,      _____,      KC_PENT, \
-        _____,                  KC_PDOT
+    [_MED] = KEYMAP(
+        _____,                _____,      _____,      KC_VOLDOWN, \
+        _____,                _____,      _____,      KC_VOLUP, \
+        _____,                _____,      _____,               \
+        _____,                _____,      _____,      KC_MUTE, \
+        _____,                            _____
     ),
 };
 
