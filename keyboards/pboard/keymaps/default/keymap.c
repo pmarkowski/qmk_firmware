c50d21d362d03d8522df460e688aba1ef58a783a
Philip <philipmarkowski642@gmail.com>
2017-05-08 22:06:48 -0400

    pboard define media layout

--- keyboards/pboard/keymaps/default/keymap.c
+++ keyboards/pboard/keymaps/default/keymap.c
@@ -23,10 +23,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = KEYMAP( \
-    F(0),    KC_1,    KC_2,    KC_3,  KC_4,   KC_5,   KC_6, KC_7,   KC_8,    KC_9,   KC_0,     KC_MINS,  KC_EQL,  KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,   KC_T,   KC_Y, KC_U,   KC_I,    KC_O,   KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, \
-    MO(1),   KC_A,    KC_S,    KC_D,  KC_F,   KC_G,   KC_H, KC_J,   KC_K,    KC_L,   KC_SCLN,  KC_QUOTE, KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,  KC_V,   KC_B,   KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,  KC_UP,   KC_DEL, \
+    F(0),    KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6, KC_7,   KC_8,    KC_9,   KC_0,     KC_MINS,  KC_EQL,  KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y, KC_U,   KC_I,    KC_O,   KC_P,     KC_LBRC,  KC_RBRC, KC_BSLS, \
+    MO(_FN), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H, KC_J,   KC_K,    KC_L,   KC_SCLN,  KC_QUOTE, KC_ENT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N, KC_M,   KC_COMM, KC_DOT, KC_SLASH, KC_RSFT,  KC_UP,   MO(_MED), \
     KC_LCTL, KC_LGUI, KC_LALT, KC_BSPC, KC_SPC,               KC_SPC, KC_RALT, KC_NO,  KC_RCTL,  KC_LEFT,  KC_DOWN, KC_RIGHT \
   ),
   [_FN] = KEYMAP( \
@@ -34,14 +34,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_CAPS,  LCTL(KC_LEFT), KC_UP,   LCTL(KC_RIGHT), ___,     ___,        KC_CALC, KC_PGUP, KC_UP,   KC_PGDN,  KC_PSCR, KC_SLCK, KC_PAUS, ___, \
     ___,      KC_LEFT,       KC_DOWN, KC_RIGHT,       KC_ENT,  ___,        KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_INS,  KC_DEL,  ___, \
     ___,      KC_APP,        KC_VOLD, KC_VOLU,        KC_MUTE, ___,        KC_END,  ___,     ___,     ___,      ___,     ___,     ___,     ___, \
-    ___,      ___,           ___,     ___,            ___,                          ___,     ___,     ___,      ___,     ___,     ___,     ___ \
+    ___,      ___,           ___,     ___,            ___,                          KC_ENT,  ___,     ___,      ___,     ___,     ___,     ___ \
   ),
   [_MED] = KEYMAP( \
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, \
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, \
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, \
-    ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, ___, \
-    ___, ___, ___, ___, ___,           ___, ___, ___, ___, ___, ___, ___ \
+    ___, ___, ___,     ___,     ___,     ___, ___, ___, ___, ___, ___, ___,     ___,     ___, \
+    ___, ___, ___,     ___,     ___,     ___, ___, ___, ___, ___, ___, ___,     ___,     ___, \
+    ___, ___, ___,     ___,     ___,     ___, ___, ___, ___, ___, ___, ___,     ___, \
+    ___, ___, KC_VOLD, KC_VOLU, KC_MUTE, ___, ___, ___, ___, ___, ___, ___,     KC_MSTP, ___, \
+    ___, ___, ___,     ___,     ___,               ___, ___, ___, ___, KC_MPRV, KC_MPLY, KC_MNXT \
   ),
 };
 
