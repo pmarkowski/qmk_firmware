811e2171991c92338e02c50a47b271c19bd17b12
Philip <philipmarkowski642@gmail.com>
2017-06-17 17:13:10 -0400

    fix plus

--- keyboards/ppad/keymaps/default/keymap.c
+++ keyboards/ppad/keymaps/default/keymap.c
@@ -18,7 +18,7 @@ enum reset_state {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = KEYMAP(
         LT(_MED, KC_NLCK),    KC_PSLS,    KC_PAST,    KC_PMNS, \
-        KC_P7,                KC_P8,      KC_P9,      KC_PSLS, \
+        KC_P7,                KC_P8,      KC_P9,      KC_PPLS, \
         KC_P4,                KC_P5,      KC_P6,               \
         KC_P1,                KC_P2,      KC_P3,      KC_ENT, \
         KC_KP_0,                          KC_PDOT
