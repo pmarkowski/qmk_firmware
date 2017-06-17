394914b523baa9041b757571420830c5f80fbbaf
Philip <philipmarkowski642@gmail.com>
2017-06-17 16:55:29 -0400

    fix 6

--- keyboards/ppad/keymaps/default/keymap.c
+++ keyboards/ppad/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = KEYMAP(
         LT(_MED, KC_NLCK),    PP_DIV,     PP_MULT,    PP_MINUS, \
         KC_P7,                KC_P8,      KC_P9,      PP_PLUS, \
-        KC_P4,                KC_P5,      KC_P8,               \
+        KC_P4,                KC_P5,      KC_P6,               \
         KC_P1,                KC_P2,      KC_P3,      PP_ENT, \
         PP_ZERO,                          KC_PDOT
     ),
